2451198f2c503bd1f0e26663c1cf5d16d7c1b0cc
Yumin Chen <yumin@chen.so>
2023-07-21 09:11:03 +0100

    Update radipan@0.0.10

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
-import { jsx } from 'radipan';
+import { createElement as h } from 'radipan';
 import ReactDOM from 'react-dom/client';
 import App from './App.ts';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  jsx(StrictMode, jsx(App))
+  h(StrictMode, {}, h(App))
 );
